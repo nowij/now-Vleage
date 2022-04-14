bf99959b8e0598907f61fd92ba298369094365f6
nowij <nowijdl@gmail.com>
2022-04-14 10:26:57 +0900

    project init

---
+++ src/main/java/com/nowij/nv/NowVleageApplication.java
@@ -0,0 +1,13 @@
+package com.nowij.nv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowVleageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowVleageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nowij/nv/NowVleageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowij.nv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowVleageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
